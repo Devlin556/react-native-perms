16b628d6f3ad8f65793eeb0a050878ae2c35d367
Alexey Maligon <fania556@gmail.com>
2018-03-23 19:45:17 +0300

    Update emitter and add PermissionLocationManager

--- react-native-perms/react-native-perms-Bridging-Header.h
+++ react-native-perms/react-native-perms-Bridging-Header.h
@@ -5,6 +5,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
+#import <React/RCTEventEmitter.h>
 #import <React/RCTRootView.h>
 #import <React/RCTUtils.h>
 #import <React/RCTConvert.h>
