ec91e73524e757b566943b190f9590376011b76b
Alexey Maligon <fania556@gmail.com>
2018-04-09 17:25:55 +0300

    Fix ReactPackage

--- android/src/main/java/com/alexey_milk/react_native_perms/ReactNativePermsReactPackage.java
+++ android/src/main/java/com/alexey_milk/react_native_perms/ReactNativePermsReactPackage.java
@@ -4,12 +4,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import android.view.ViewManager;
 
 public class ReactNativePermsReactPackage implements ReactPackage {
 
@@ -17,7 +16,8 @@ public class ReactNativePermsReactPackage implements ReactPackage {
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
-
+    
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
