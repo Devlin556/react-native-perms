e1d4a50743c9b4e4d00a0a3fd6786e068a4c2172
Alexey Maligon <fania556@gmail.com>
2018-04-11 18:45:16 +0300

    Update uri and import URI package

--- android/src/main/java/com/alexey_milk/react_native_perms/ReactNativePermsModule.java
+++ android/src/main/java/com/alexey_milk/react_native_perms/ReactNativePermsModule.java
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import android.net.Uri;
 import android.provider.Settings;
 import android.content.Intent;
 
@@ -24,7 +25,7 @@ public class ReactNativePermsModule extends ReactContextBaseJavaModule {
     public void openSettings() {
         Intent settings = new Intent();
         settings.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        Uri uri = Uri.fromParts("package", reactContext.getPackageName(), null);
         settings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         settings.setData(uri);
         reactContext.startActivity(settings);
