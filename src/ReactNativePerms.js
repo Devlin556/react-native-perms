6e92bdc0a824657c145742afc72c8dd9a2e815ec
Alexey Maligon <fania556@gmail.com>
2018-04-11 19:18:24 +0300

    Update status for android permissions

--- src/ReactNativePerms.js
+++ src/ReactNativePerms.js
@@ -73,7 +73,7 @@ export default class ReactNativePerms {
           if (res) {
             resolve('authorizedWhenInUse');
           } else {
-            resolve(null);
+            resolve('undetermined');
           }
         })
       );
